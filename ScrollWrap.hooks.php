824754989948693eec97d73d1b6369e47a945c2f
Jason Tamez <jasontamez@users.noreply.github.com>
2016-10-11 00:03:39 -0500

    CRLF to LF
    Line breaks?

--- ScrollWrap.hooks.php
+++ ScrollWrap.hooks.php
@@ -12,8 +12,7 @@ class ScrollWrapHooks {
 		$parser->setFunctionHook( 'something', 'ScrollWrapHooks::doSomething' );
 	}
 
-	public static function doSomething( Parser &$parser )
-	{
+	public static function doSomething( Parser &$parser ) {
 		// Called in MW text like this: {{#something: }}
 
 		// For named parameters like {{#something: foo=bar | apple=orange | banana }}
